4ba214ae17b81939a81e83ba7baf810338e19fdb
Hector Sandoval <hesandova@gmail.com>
2015-09-15 20:55:55 -0700

    Calculator requirements done

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,97 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var historyDisplay: UILabel!
+    @IBOutlet weak var display: UILabel!
+    var typingNumber = false
+    var validDecimal = true
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        println("digit = \(digit)")
+        if typingNumber{
+            if digit == "."{
+                if validDecimal{
+                    display.text = display.text! + digit
+                    validDecimal = false
+                }
+            }else{
+                display.text = display.text! + digit
+            }
+            
+        }else{
+            display.text = digit
+            if digit == "."{
+                validDecimal = false
+            }
+            typingNumber = true
+        }
+        
+    }
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        typingNumber = false
+        validDecimal = true
+        historyDisplay.text = historyDisplay.text! + " \(displayValue) "
+        operandStack.append(displayValue)
+        println("Operand Stack = \(operandStack)")
+        
+    }
+    
+
+    var displayValue: Double {
+        get{
+           return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+            typingNumber = false
+        }
+    }
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if typingNumber{
+            enter()
+        }
+        historyDisplay.text = historyDisplay.text! + " \(operation) "
+        switch operation{
+        case "×":performOperation {$0 * $1}
+        case "÷":performOperation {$1 / $0}
+        case "+":performOperation {$0 + $1}
+        case "-":performOperation {$1 - $0}
+        case "√":perform { sqrt($0) }
+        case "sin": perform { sin($0) }
+        case "cos":perform { cos($0) }
+        case "π": piAction()
+        default: break
+            
+        }
+    }
+    func piAction() {
+        display.text = "\(M_PI)"
+    }
+    
+    func perform(operation: Double->Double ){
+        if operandStack.count >= 1{
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperation(operation: (Double, Double)->Double ){
+        if operandStack.count >= 2{
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    @IBAction func clear() {
+        display.text = ""
+        historyDisplay.text = ""
+        operandStack = Array<Double>()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
